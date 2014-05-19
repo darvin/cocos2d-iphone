c99f5f4e2b43fef165436e525a629f54cd37e0eb
Oleg Osin <olegosin@Olegs-MacBook-Pro-2.local>
2014-05-19 11:28:55 -0700

    Added score label began work on background scene.

--- cocos2d-ui-tests/tests/CCEffectPongTest.m
+++ cocos2d-ui-tests/tests/CCEffectPongTest.m
@@ -14,8 +14,7 @@
 
 #if CC_ENABLE_EXPERIMENTAL_EFFECTS
 
-#define PADDLE_HEIGHT 100.0f
-#define PADDLE_WIDTH 20.0f
+#define PADDLE_SCALE 0.5f
 #define PADDLE_X_OFFSET 10.0f
 
 #define BALL_HEIGHT 10.0f
@@ -24,13 +23,15 @@
 #define CEILING_HEIGHT 5.0f
 #define FLOOR_HEIGHT 5.0f
 
+#define BALL_VELOCITY 200.0f
+
 typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
 
 @interface CCEffectPongTest : TestBase <CCPhysicsCollisionDelegate> @end
 
 @implementation CCEffectPongTest {
-    CCNodeColor* _playerPaddle;
-    CCNodeColor* _aiPaddle;
+    CCSprite* _playerPaddle;
+    CCSprite* _aiPaddle;
     CCSprite* _ball;
     CGSize _designSize;
     
@@ -43,13 +44,19 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
     CCEffectNode* _pixellateEffectNode;
     CCEffectPixellate* _pixellateEffect;
     
+    CCLabelTTF* _scoredLabel;
+    
     TEST_PONG_STATE _gameState;
+    
+    int _playerScore;
+    int _aiScore;
 }
 
 - (BOOL)canPaddleMoveTo:(float)y
 {
-    return !((y + (PADDLE_HEIGHT * 0.5f) > _designSize.height - CEILING_HEIGHT) ||
-            (y - (PADDLE_HEIGHT * 0.5f) < FLOOR_HEIGHT));
+    const float paddleHeight = _playerPaddle.contentSize.height * PADDLE_SCALE;
+    return !((y + (paddleHeight * 0.5f) > _designSize.height - CEILING_HEIGHT) ||
+            (y - (paddleHeight * 0.5f) < FLOOR_HEIGHT));
 }
 
 - (void)setupEffectPongTest
@@ -65,7 +72,18 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
     
     _designSize = [[CCDirector sharedDirector] designSize];
     _designSize.height -= _headerBg.contentSize.height;
+    
+    _pixellateEffectNode = [[CCEffectNode alloc] initWithWidth:_designSize.width height:_designSize.height];
+    
+    // HACK
+    // This should work with position 0,0. FIXME - Oleg. An effectnodes position without an actual
+    // effect handles behave different when an effect is added.
+    _pixellateEffectNode.position = ccp(_designSize.width * 0.5f, _designSize.height * 0.5f);
+    
+    _pixellateEffect = [[CCEffectPixellate alloc] initWithPixelScale:0.0001f];
 
+    [self setupBackgroundScene];
+    
     [self setupBall];
     [physics addChild:_ballEffectNode];
 
@@ -75,17 +93,11 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
     [self setupAIPaddle];
     [physics addChild:_aiPaddle];
     
-    
     [self setupFloorAndCeiling];
     [physics addChild:_ceiling];
     [physics addChild:_floor];
     
-    _pixellateEffectNode = [[CCEffectNode alloc] initWithWidth:_designSize.width height:_designSize.height];
-    
-    // HACK
-    _pixellateEffectNode.position = ccp(_designSize.width * 0.5f, _designSize.height * 0.5f); // This should work with position 0,0. FIXME - Oleg. An effectnodes position without an actual effect handles behave different when an effect is added.
-    
-    _pixellateEffect = [[CCEffectPixellate alloc] initWithPixelScale:0.0001f];
+    [self setupScoredLabel];
     
     [_pixellateEffectNode addChild:physics];
 
@@ -99,9 +111,9 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
     // Left paddle (player)
     if(_playerPaddle == nil)
     {
-        _playerPaddle = [CCNodeColor nodeWithColor:[CCColor redColor]];
+        _playerPaddle = [CCSprite spriteWithImageNamed:@"sample_vertical_rect.png"];
         _playerPaddle.anchorPoint = ccp(0.0, 0.5);
-        _playerPaddle.contentSize = CGSizeMake(PADDLE_WIDTH, PADDLE_HEIGHT);
+        _playerPaddle.scale = PADDLE_SCALE;
         
         CGRect rect = {CGPointZero, _playerPaddle.contentSize};
         _playerPaddle.physicsBody = [CCPhysicsBody bodyWithRect:rect cornerRadius:0.0];
@@ -116,9 +128,9 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
 {
     if(_aiPaddle == nil)
     {
-        _aiPaddle = [CCNodeColor nodeWithColor:[CCColor redColor]];
+        _aiPaddle = [CCSprite spriteWithImageNamed:@"sample_vertical_rect.png"];
         _aiPaddle.anchorPoint = ccp(0.0, 0.5);
-        _aiPaddle.contentSize = CGSizeMake(PADDLE_WIDTH, PADDLE_HEIGHT);
+        _aiPaddle.scale = PADDLE_SCALE;
         
         CGRect rect = {CGPointZero, _aiPaddle.contentSize};
         _aiPaddle.physicsBody = [CCPhysicsBody bodyWithRect:rect cornerRadius:0.0];
@@ -126,7 +138,7 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
         _aiPaddle.physicsBody.collisionType = @"aiPaddle";
     }
     
-    _aiPaddle.position = ccp(_designSize.width - PADDLE_WIDTH - PADDLE_X_OFFSET, _designSize.height * 0.5f);
+    _aiPaddle.position = ccp(_designSize.width - _aiPaddle.contentSize.width - PADDLE_X_OFFSET, _designSize.height * 0.5f);
 }
 
 - (void)setupBall
@@ -152,7 +164,7 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
         [_ballEffectNode addEffect:_ballEffect];
     }
     
-    _ballEffectNode.physicsBody.velocity = ccp(-160, 0);
+    _ballEffectNode.physicsBody.velocity = ccp(-BALL_VELOCITY, 0);
     _ballEffectNode.physicsBody.angularVelocity = 0.1;
     _ballEffectNode.position = ccp(_designSize.width * 0.5f, _designSize.height * 0.5f);
 }
@@ -182,6 +194,22 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
     _floor.position = ccp(_designSize.width * 0.5f, 0.0f);
 }
 
+- (void)setupBackgroundScene
+{
+    CCSprite* beigePannel = [CCSprite spriteWithImageNamed:@"panelInset_beige.png"];
+    beigePannel.positionType = CCPositionTypeNormalized;
+    beigePannel.position = ccp(0.3, 0.3);
+    
+    [_pixellateEffectNode addChild:beigePannel];
+}
+
+- (void)setupScoredLabel
+{
+    _scoredLabel = [CCLabelTTF labelWithString:@"Score %i/%i" fontName:@"HelveticaNeue-Medium" fontSize:17 * [CCDirector sharedDirector].UIScaleFactor];
+    _scoredLabel.positionType = CCPositionTypeNormalized;
+    _scoredLabel.position = ccp(0.5f,0.5f);
+}
+
 - (void)sceneUpdate:(CCTime)interval
 {
     if(_gameState == TEST_PONG_PLAYING)
@@ -208,24 +236,45 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
     {
         _gameState = TESTS_PONG_GAMEOVER;
         
-        // HACK
-        _pixellateEffectNode.position = ccp(0.0f, 0.0f); // FIXME - Oleg. An effectnodes position without an actual effect handles behave different when an effect is added.
+        [self updateScore];
+        
+        // FIXME - Oleg. An effectnodes position without an actual effect handles behave different when an effect is added.
+        _pixellateEffectNode.position = ccp(0.0f, 0.0f);
         [_pixellateEffectNode addEffect:_pixellateEffect];
         
-        [self schedule:@selector(increasePixellate:) interval:1.0f/60.0f repeat:10 delay:0.0f];
-        [self schedule:@selector(decreasePixellate:) interval:1.0f/60.0f repeat:10 delay:0.1f];
-        [self scheduleOnce:@selector(resetGame:) delay:0.5f];
+//        [self setupBall];
+//        _ballEffectNode.physicsBody.velocity = ccp(0, 0);
+        
+        [_pixellateEffectNode addChild:_scoredLabel];
+        
+        [self schedule:@selector(increasePixellate:) interval:1.0f/60.0f repeat:10 delay:1.0f];
+        [self schedule:@selector(decreasePixellate:) interval:1.0f/60.0f repeat:10 delay:2.0f];
+        [self scheduleOnce:@selector(resetGame:) delay:3.0f];
     }
+}
+
+- (void)updateScore
+{
+    if(_ballEffectNode.position.x > _designSize.width * 0.5f)
+        _playerScore++;
+    else
+        _aiScore++;
+    
+    _scoredLabel.string = [NSString stringWithFormat:@"Score %i/%i", _playerScore, _aiScore];
 }
 
 - (void)resetGame:(CCTimer*)interval
 {
     [self setupPlayerPaddle];
     [self setupAIPaddle];
-    [self setupBall];
 
     // FIXME - make sure effect node reverts back to acting as empty node after all effects have been removed
     [_pixellateEffectNode removeEffect:_pixellateEffect];
+  
+    [self setupBall];
+//    _ballEffectNode.physicsBody.velocity = ccp(-BALL_VELOCITY, 0);
+    
+    [_pixellateEffectNode removeChild:_scoredLabel];
     
     _gameState = TEST_PONG_PLAYING;
 }
@@ -268,12 +317,12 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
 
 - (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair playerPaddle:(CCNode *)playerPaddle ball:(CCNode *)ball
 {
-    [ball.physicsBody applyImpulse:ccp(10, 0.0)];
+    [ball.physicsBody applyImpulse:ccp(BALL_VELOCITY * 0.5f, 0.0)];
 }
 
 - (void)ccPhysicsCollisionSeparate:(CCPhysicsCollisionPair *)pair playerPaddle:(CCNode *)playerPaddle ball:(CCNode *)ball
 {
-    [ball.physicsBody applyImpulse:ccp(100, 0.0)];
+    [ball.physicsBody applyImpulse:ccp(BALL_VELOCITY, 0.0)];
     
     [self schedule:@selector(increaseBallBlur:) interval:1.0f/60.0f repeat:10 delay:0.0f];
     [self schedule:@selector(decreaseBallBlur:) interval:1.0f/60.0f repeat:10 delay:0.1f];
@@ -283,12 +332,12 @@ typedef enum { TEST_PONG_PLAYING, TESTS_PONG_GAMEOVER } TEST_PONG_STATE;
 
 - (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair aiPaddle:(CCNode *)aiPaddle ball:(CCNode *)ball
 {
-    [ball.physicsBody applyImpulse:ccp(-10, 0.0)];
+    [ball.physicsBody applyImpulse:ccp(-BALL_VELOCITY * 0.5f, 0.0)];
 }
 
 - (void)ccPhysicsCollisionSeparate:(CCPhysicsCollisionPair *)pair aiPaddle:(CCNode *)aiPaddle ball:(CCNode *)ball
 {
-    [ball.physicsBody applyImpulse:ccp(-100, 0.0)];
+    [ball.physicsBody applyImpulse:ccp(-BALL_VELOCITY, 0.0)];
     
     [self schedule:@selector(increaseBallBlur:) interval:1.0f/60.0f repeat:10 delay:0.0f];
     [self schedule:@selector(decreaseBallBlur:) interval:1.0f/60.0f repeat:10 delay:0.1f];
